f25813f248b5282e954c8983191a1ddebb4b11e4
JohnQuezada <johnaquezada@gmail.com>
2022-01-25 18:37:24 -0500

    Car rental project

---
+++ src/com/careerdevs/Car.java
@@ -0,0 +1,43 @@
+package com.careerdevs;
+
+public class Car {
+    // Create the following fields:
+    //make (String)
+    //model (String)
+    //isRented (boolean)
+    //[Heads up: this class will be expanded with many more fields later on)
+    //Create a constructor that uses on the fields of Car
+    //Create a toString method (you can use IntelliJ’s generator)
+
+    private String make;
+    private String model;
+    private boolean isRented;
+
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+        this.isRented = false;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getName() {
+        return getMake() + " " + getModel();
+    }
+
+    public void setRented(boolean rented) {
+        isRented = rented;
+    }
+
+    public boolean isRented() {
+        return isRented;
+
+
+    }
+}
---
+++ src/com/careerdevs/RentalService.java
@@ -0,0 +1,37 @@
+package com.careerdevs;
+
+import java.util.Scanner;
+
+public class RentalService {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Car Rental CLI");
+
+//        Car[] carStorage = new Car[3];
+//
+//        Car car1 = new Car();
+//        Car car2 = new Car();
+//        Car car3 = new Car();
+//
+//        carStorage[0] = car1;
+//        carStorage[1] = car2;
+//        carStorage[2] = car3;
+//
+//
+//        System.out.println("Available Cars:");
+//        int listNumber = 1;
+//
+//        for (int i = 0; i < carStorage.length; i++) {
+//            if ( !carStorage[i].isRented() ) {
+//                System.out.println( "(" + (listNumber) + ") " + carStorage[i].getName());
+//                listNumber++
+//            }
+//        }
+
+
+
+
+
+    }
+}
