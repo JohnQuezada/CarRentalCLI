a62dcb08a8da94eba53a887623afcf294e9aeda5
JohnQuezada <johnaquezada@gmail.com>
2022-02-10 21:38:21 -0500

    Car rental project CLI update

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         CLI.readInt("enter a number", 1, 10);
-
+    // work om CLI this weekend
     }
 }
---
+++ src/com/careerdevs/UI.java
@@ -0,0 +1,18 @@
+package com.careerdevs;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class UI {
+    private static Scanner scanner = new Scanner(System.in);
+    public static int readInt (String question, int min, int max) {
+
+//        try {
+//
+//        } catch (InputMismatchException exception) {
+//
+//        }
+
+        System.out.println();
+    }
+}
